52bde536b218d94b827720c8c39fecbf4a78df59
Rohit Saxena <rohitsaxena722@gmail.com>
2022-01-09 19:39:10 +0530

    0.2 base_ip code

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
-from flask import Flask
+import urllib.request
 
-app = Flask(__name__)
+import requests
+from flask import Flask as flask
+import json
+
+app = flask(__name__)
 
 
 @app.route("/")
 def home_view():
-    return "<h1>Heroku test successfull</h1>"
+    ipur = 'https://ipapi.co/'
+    ipurr = '/json/'
+    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    print('Target IP Addr is : ' + external_ip)
+    ip_address = external_ip
+    # URL to send the request to
+    request_url = ipur + external_ip + ipurr
+    # Send request and decode the result
+    response = requests.get(request_url)
+    result = response.content.decode()
+    result = json.loads(result)
+    test = json.dumps(result)
+    print(test)
+    return test
+
+
+@app.route("/geoip")
+def home_():
+    return 'test'
